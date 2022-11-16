62b93540df0be4ff7fe69529678b662912705049
Denis Saenko <denissaenkoo@gmail.com>
2022-11-16 18:26:46 +0300

    updated to pass id type

--- src/typeorm/types/entities/base-soft-removable.entity.ts
+++ src/typeorm/types/entities/base-soft-removable.entity.ts
@@ -1,7 +1,9 @@
 import { DeleteDateColumn } from "typeorm";
 import { BaseEntity } from "./base.entity";
 
-export abstract class BaseSoftRemovableEntity extends BaseEntity {
+export abstract class BaseSoftRemovableEntity<
+  TId = number
+> extends BaseEntity<TId> {
   @DeleteDateColumn()
   deletedAt: Date;
 }
--- src/typeorm/types/entities/base.entity.ts
+++ src/typeorm/types/entities/base.entity.ts
@@ -1,16 +1,3 @@
-import {
-  CreateDateColumn,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn
-} from "typeorm";
-
-export abstract class BaseEntity {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @CreateDateColumn()
-  createdAt: Date;
-
-  @UpdateDateColumn()
-  updatedAt: Date;
+export abstract class BaseEntity<TId = number> {
+  abstract id: TId;
 }
