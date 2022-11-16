6d2831670377a27c34263935068577cac003cf56
Denis Saenko <denissaenkoo@gmail.com>
2022-11-16 18:28:35 +0300

    module updated

--- src/typeorm/typeorm.module.ts
+++ src/typeorm/typeorm.module.ts
@@ -1,21 +1,32 @@
 import { Module } from "@nestjs/common";
+import { CategoryRepository } from "../category/types";
 import { CustomConfigModule, DataBaseConfig } from "../core/config";
 import { CustomLoggerModule } from "../core/custom-logger";
 import { DefaultConnection } from "./connections";
+import { TypeormCategoryRepository } from "./repositories";
+import { Connection } from "./types";
+import { IntegerIdValidation } from "./validations";
+import { UUIDValidation } from "./validations/uuid.validation";
 
 @Module({
   imports: [CustomConfigModule, CustomLoggerModule],
   providers: [
+    UUIDValidation,
+    IntegerIdValidation,
     {
-      provide: DefaultConnection,
+      provide: Connection,
       useFactory: async function (config: DataBaseConfig) {
         const connection = new DefaultConnection(config);
         await connection.dataSource.initialize();
         return connection;
       },
       inject: [DataBaseConfig]
+    },
+    {
+      provide: CategoryRepository,
+      useClass: TypeormCategoryRepository
     }
   ],
-  exports: []
+  exports: [CategoryRepository, UUIDValidation]
 })
 export class TypeormModule {}
